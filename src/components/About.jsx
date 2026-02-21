e6e273c84731c6a571a0af014391980dcc1bbb8c
Misha2007 <55316381+Misha2007@users.noreply.github.com>
2026-02-21 18:52:19 +0200

    Add emphasis to key phrases in About component
    Enhanced text presentation by adding emphasis to key phrases.

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -17,29 +17,33 @@ export default function About() {
           in the vocational school. Before moving abroad, I attended gymnasium
           Kyiv-Mohyla Collegium in <strong>Ukraine</strong>. 
         </p>
+        <br>
         <p className="text-darkgray dark:text-gray text-lg leading-relaxed">
           I decided to become a
           software developer because this speciality opens a lot of
-          opportunities and you can be as creative as you want. My first
-          experience with coding came through a website building course and
+          opportunities and you can be <strong>as creative as you want</strong>. My first
+          experience with coding came through a <strong>website building course</strong> and
           after working on a few projects, I realized that software development
-          was the perfect field for me. The more I learned, the more I felt
+          was the <strong>perfect field</strong> for me. The more I learned, the more I felt
           motivated to continue pursuing this path. 
         </p>
+        <br>
         <p className="text-darkgray dark:text-gray text-lg leading-relaxed">
           Even though I haven't
           participated in any official competitions yet, I took part in the <strong>Hack Club's
           Neighborhood program</strong>. The goal of the program was to build a project
-          over 100 hours and potentially get invited to San Francisco. Although
+          over <strong>100 hours</strong> and potentially get invited to San Francisco. Although
           the project was closed before I could complete it, I received valuable
           feedback from testers and learned a lot through the process. 
         </p>
+        <br>
         <p className="text-darkgray dark:text-gray text-lg leading-relaxed">
           Outside
           of programming, I enjoy playing <strong>volleyball</strong> and <strong>ping pong</strong>. I also like
           to play the <strong>guitar</strong> in my free time; it's a great way to relax and
           express creativity in a different way. 
         </p>
+        <br>
         <p className="text-darkgray dark:text-gray text-lg leading-relaxed">
           Thank you for visiting my
           portfolio, and feel free to explore my work!
